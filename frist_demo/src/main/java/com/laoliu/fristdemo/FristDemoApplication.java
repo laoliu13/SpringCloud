5f288573f2bf7986382baba50bbff0bbed04ff63
liuzhijun <liuzhijun931030@gmail.com>
2023-09-24 17:08:06 +0800

    第一个SpringCloudAlibaba程序

---
+++ frist_demo/src/main/java/com/laoliu/fristdemo/FristDemoApplication.java
@@ -0,0 +1,13 @@
+package com.laoliu.fristdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FristDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FristDemoApplication.class, args);
+    }
+
+}
